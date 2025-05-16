6deb052dcef3b2d7ca4308209ed29a447e4315b4
Jacob1539 <jacob.witt.100@gmail.com>
2025-05-15 23:21:24 -0400

    Github pages fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Homepage from './pages/Homepage'
 import Rules from './pages/Rules'
 import Scoring from './pages/Scoring'
